efd1932207fbe1ad1be87fea3e9216e77a00ec15
minettearmada <114899727+minettearmada@users.noreply.github.com>
2024-03-30 09:48:16 +0800

    Sign up working in firebase

---
+++ app/src/main/java/com/mobdeve/s13/grp7/pokelearn/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.grp7.pokelearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_page)
+    }
+}

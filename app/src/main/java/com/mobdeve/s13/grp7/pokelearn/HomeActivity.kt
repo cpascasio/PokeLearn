c976d1cff52cde066768f94b9a080428c56ce49b
minettearmada <114899727+minettearmada@users.noreply.github.com>
2024-04-01 01:51:57 +0800

    Removed username and name in profile

--- app/src/main/java/com/mobdeve/s13/grp7/pokelearn/HomeActivity.kt
+++ app/src/main/java/com/mobdeve/s13/grp7/pokelearn/HomeActivity.kt
@@ -2,9 +2,18 @@ package com.mobdeve.s13.grp7.pokelearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 // HomeActivity.kt
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_home)
